73b2096a1bb8643dbfbf53fc3c7b6b34fed7556b
Arash Parnia <arashparnia@gmail.com>
2015-10-29 11:58:08 -0700

    polyline page edit button working

--- TAMS/PolyLineViewController.swift
+++ TAMS/PolyLineViewController.swift
@@ -30,41 +30,57 @@ class PolyLineViewController: UIViewController,MKMapViewDelegate,UIGestureRecogn
         
         mapView.delegate = self
         mapView.userInteractionEnabled = false
+        
+//        let editBarButtonItem:UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Bookmarks, target: self, action: "listTapped:")
+//       
+//        self.navigationItem.setRightBarButtonItems([rightListBarButtonItem,rightSearchBarButtonItem], animated: true)
+
+        self.navigationItem.setRightBarButtonItem(self.editButtonItem(), animated: true)
+//
+//        self.navigationItem.rightBarButtonItem!.target = self;
+//        //self.navigationItem.rightBarButtonItem!.action = @selector(actionForTap:);
+//        self.navigationItem.rightBarButtonItem!.target .
+//            //addTarget(self, action: "actioncall", forControlEvents: UIControlEvents.TouchUpInside)
     
+        
         super.viewDidLoad()
     }
+    override func setEditing(editing: Bool, animated: Bool) {
+        if editing {mapView.userInteractionEnabled  = false}
+        else {mapView.userInteractionEnabled  = true}
+        super.setEditing(editing, animated: animated)
+    }
+    
     @IBOutlet weak var mapView: MKMapView!
+    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        if event?.allTouches()?.count > 1 {
-            mapView.userInteractionEnabled  = true
-        } else {
-            mapView.userInteractionEnabled  = false
-        }
-
+        if editing{
             let overlays = mapView.overlays
             mapView.removeOverlays(overlays)
-
+        }
     }
     
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if editing {
+        
         for touch in touches {
             points.append(mapView.convertPoint(touch.locationInView(mapView), toCoordinateFromView: self.mapView))
         }
         let polyline =  MKPolyline(coordinates: &points, count: points.count)
         mapView.addOverlay(polyline)
+        }
     }
     
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-      
-            mapView.userInteractionEnabled  = false
-        
+        if editing {
         let overlays = mapView.overlays
         mapView.removeOverlays(overlays)
         let polyline =  MKPolyline(coordinates: &points, count: points.count)
         mapView.addOverlay(polyline)
         points.removeAll()
+        }
     }
     
     func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
